d97e1143f60f9015febd1a7d735d6c2f7711ffe0
Nishikantaray <nishikantaray1@gmail.com>
2022-06-28 16:26:14 +0530

    initial commite

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import Header from './Header';
+import AddContactList from './AddContactList';
+import ContactList from './ContactList';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <AddContactList/>
+    <ContactList/>
+    
+    </>
+  );
+}
+
+export default App;
